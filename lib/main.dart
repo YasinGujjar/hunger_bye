2d2c10d6a5a95c9574b0f32adb38afc811fc4885
aifrosals <a.ifrosals@gmail.com>
2020-01-15 12:58:13 +0500

    Card for request

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hunger_bye/ui/requests.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,7 +21,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Dashboard(),
+      //MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
