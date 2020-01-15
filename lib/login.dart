5c4a14e3faa75373044c1099b66ce10580a51cf6
YasinGujjar <muhammadyasin9922@gmail.com>
2020-01-15 16:25:26 +0500

    login update

--- lib/login.dart
+++ lib/login.dart
@@ -14,182 +14,205 @@ class _LoginState extends State<Login> {
    TextEditingController passwordcontroller=TextEditingController();
   @override
   Widget build(BuildContext context) {
+    double width=MediaQuery.of(context).size.width;
+    double height=MediaQuery.of(context).size.height;
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
+        width: width,
+        height: height,
+       /*  decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage("assets/image1.png"),
             fit: BoxFit.cover,
             alignment: Alignment.topCenter
           )
+        ), */
+
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 2,
+              child: Container(
+                  decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/image1try.png"),
+            fit: BoxFit.fill,
+            alignment: Alignment.topCenter
+          )
         ),
-        child: Form(
-          child: ListView(
-            padding: EdgeInsets.all(10),
-            children: <Widget>[
-              SizedBox(
-                height: MediaQuery.of(context).size.height/2.15,
-              ),
-              TextFormField(
-                controller: emailcontroller,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontFamily: "CentraleSansRegular"
-                ),
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(
-                      color: Color(0xffd3dde4),
-                      width: 3
-                    )
-                  ),
-                  labelText: "Email",
-                  labelStyle: TextStyle(
-                    color: Colors.black,
-                    fontSize: 17,
-                    fontFamily: "CentraleSansRegular"
-                  )
-                ),
-              ),
-              SizedBox(height: 20,),
-              TextFormField(
-                controller: passwordcontroller,
-                obscureText: true,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontFamily: "CentraleSansRegular"
-                ),
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(
-                      color: Color(0xffd3dde4),
-                      width: 3
-                    )
-                  ),
-                  labelText: "Password",
-                  labelStyle: TextStyle(
-                    color: Colors.black,
-                    fontSize: 17,
-                    fontFamily: "CentraleSansRegular"
-                  )
-                ),
-              ),
-              SizedBox(height: 20,),
-              Container(
-                
-                child: Material(
-                  
-                                child: InkWell(
-                    onTap: (){
-                      print("clicked");
-                    },
-                                  child: Container(
-                      width: 330,
-                      height: 60,
+              )),
+            Expanded(
+              flex: 4,
+                          child: Form(
+                child: ListView(
+                  padding: EdgeInsets.all(10),
+                  children: <Widget>[
+                   /*  SizedBox(
+                      height: MediaQuery.of(context).size.height/2.15,
+                    ), */
+                    TextFormField(
+                      controller: emailcontroller,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: "CentraleSansRegular"
+                      ),
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                            color: Color(0xffd3dde4),
+                            width: 3
+                          )
+                        ),
+                        labelText: "Email",
+                        labelStyle: TextStyle(
+                          color: Colors.black,
+                          fontSize: 17,
+                          fontFamily: "CentraleSansRegular"
+                        )
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+                    TextFormField(
+                      controller: passwordcontroller,
+                      obscureText: true,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: "CentraleSansRegular"
+                      ),
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(
+                            color: Color(0xffd3dde4),
+                            width: 3
+                          )
+                        ),
+                        labelText: "Password",
+                        labelStyle: TextStyle(
+                          color: Colors.black,
+                          fontSize: 17,
+                          fontFamily: "CentraleSansRegular"
+                        )
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+                    Container(
                       
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [
-                            Color(0xff471a91),
-                            Color(0xff3cabff)
-                          ],
+                      child: Material(
+                        
+                                      child: InkWell(
+                          onTap: (){
+                            print("clicked");
+                          },
+                                        child: Container(
+                            width: 330,
+                            height: 60,
+                            
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [
+                                  Color(0xff471a91),
+                                  Color(0xff3cabff)
+                                ],
+                              ),
+                            ),
+                            child: Center(
+                              child: Text(
+                                "Login",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: "CentraleSansRegular",
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold
+                                ),
+                              ),
+                            ),
+                          ),
                         ),
                       ),
-                      child: Center(
-                        child: Text(
-                          "Login",
+                    ),
+                    SizedBox(height: 20,),
+                    Center(child: Text("Forgot Password?",
+                    style: TextStyle(
+                      fontFamily: 'CentraleSansRegular',
+                      fontSize: 17,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600
+                      ),)),
+                    SizedBox(height: 20,),
+                    MaterialButton(
+                      minWidth: 330,
+                      height: 70,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        side: BorderSide(
+                          color: Color(0xffD44638),
+                          width: 3
+                        )
+                      ),
+                      onPressed: ()
+                      {
+                         Navigator.push(context, MaterialPageRoute(builder: (context)=>Register()));
+                      },
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Icon(LineAwesomeIcons.google,
+                          color: Color(0xffD44638),
+                          size: 40,),
+                          Text(
+                          "Connect with Google",
                           style: TextStyle(
-                            color: Colors.white,
+                            color: Color(0xffD44638),
                             fontFamily: "CentraleSansRegular",
                             fontSize: 18,
                             fontWeight: FontWeight.bold
                           ),
                         ),
+                        ],
                       ),
                     ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 20,),
-              Center(child: Text("Forgot Password?",
-              style: TextStyle(
-                fontFamily: 'CentraleSansRegular',
-                fontSize: 17,
-                color: Colors.black,
-                fontWeight: FontWeight.w600
-                ),)),
-              SizedBox(height: 20,),
-              MaterialButton(
-                minWidth: 330,
-                height: 70,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  side: BorderSide(
-                    color: Color(0xffD44638),
-                    width: 3
-                  )
-                ),
-                onPressed: ()
-                {
-                   Navigator.push(context, MaterialPageRoute(builder: (context)=>Register()));
-                },
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Icon(LineAwesomeIcons.google,
-                    color: Color(0xffD44638),
-                    size: 40,),
-                    Text(
-                    "Connect with Google",
-                    style: TextStyle(
-                      color: Color(0xffD44638),
-                      fontFamily: "CentraleSansRegular",
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold
+                    SizedBox(height: 20,),
+                    MaterialButton(
+                      minWidth: 330,
+                      height: 70,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        side: BorderSide(
+                          color: Color(0xff501396),
+                          width: 3
+                        )
+                      ),
+                      onPressed: (){
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
+                      },
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Text(
+                          "Continue as Guest",
+                          style: TextStyle(
+                            color: Color(0xff501396),
+                            fontFamily: "CentraleSansRegular",
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        ],
+                      ),
                     ),
-                  ),
                   ],
                 ),
               ),
-              SizedBox(height: 20,),
-              MaterialButton(
-                minWidth: 330,
-                height: 70,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  side: BorderSide(
-                    color: Color(0xff501396),
-                    width: 3
-                  )
-                ),
-                onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()));
-                },
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Text(
-                    "Continue as Guest",
-                    style: TextStyle(
-                      color: Color(0xff501396),
-                      fontFamily: "CentraleSansRegular",
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold
-                    ),
-                  ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
